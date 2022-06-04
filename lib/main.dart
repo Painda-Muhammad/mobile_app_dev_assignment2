c8218b9f0db3123f66875f5c4265dc498ad6dda9
Painda Muhammad <khanpaindamuhammad@gmail.com>
2022-06-03 17:40:10 -0700

    assignment_2

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,97 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-Car car1 = Car("Nissan", model: "350Z", color: "yellow");
-Car car2 = Car("Nissan", color: "red");
-Car car3 = Car("Nissan");
+void main() => runApp(App());
 
-class Car {
-  String make;
-  String model;
-  String color;
-  Car(this.make, {this.model = '', this.color = ''}) {
-    print('${make}${model}${color}');
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text("Add Values in String Array in Flutter"),
+            centerTitle: true,
+          ),
+          body: SafeArea(
+              child: Center(
+            child: AppWidget(),
+          ))),
+    );
   }
-  String getOptional(String str) {
-    return str == null ? "" : " " + str;
+}
+
+class AppWidget extends StatefulWidget {
+  @override
+  AppWidgetState createState() => AppWidgetState();
+}
+
+class AppWidgetState extends State<AppWidget> {
+  final textFieldValueHolder = TextEditingController();
+
+  final List<String> data = [];
+
+  showAlert(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: new Text('Please Enter Value in Text Field.'),
+          actions: <Widget>[
+            FlatButton(
+              child: new Text("OK"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void addValue() {
+    if (textFieldValueHolder.text == '') {
+      showAlert(context);
+    } else {
+      setState(() {
+        data.add(textFieldValueHolder.text);
+      });
+      print(data);
+    }
+  }
+
+  Widget build(BuildContext context) {
+    return Center(
+        child: SingleChildScrollView(
+            child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+            width: 280,
+            padding: EdgeInsets.all(10.0),
+            child: TextField(
+              controller: textFieldValueHolder,
+              autocorrect: true,
+              decoration: InputDecoration(hintText: 'Enter Some Text Here'),
+            )),
+        Container(
+          margin: const EdgeInsets.all(10),
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              primary: Colors.lightBlue,
+              padding: EdgeInsets.all(12),
+              textStyle: TextStyle(fontSize: 22),
+            ),
+            child: Text('Add Value from input'),
+            onPressed: addValue,
+          ),
+        ),
+        Text(
+          '${data.join("\n\n" + " ")}',
+          style: TextStyle(fontSize: 20, color: Colors.black),
+          textAlign: TextAlign.center,
+        )
+      ],
+    )));
   }
 }
